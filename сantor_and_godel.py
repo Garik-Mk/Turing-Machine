488efd3890e55af8b744f2d825964472b8214c66
Garik-Mk <garikmkrtchyan277353@gmail.com>
2022-11-04 21:46:09 +0400

    Add Cantors pairing function

--- сantor_and_godel.py
+++ сantor_and_godel.py
@@ -1,20 +1,27 @@
+from math import sqrt
 
-def cantor_pairing_function(vector: tuple) -> int:
-    x, y = vector
-    
+def cantor_pairing_function(x: int, y:int) -> int:
     number = (x + y + 1) * (x + y)
     number //= 2
     number += y
-    
     return number
 
 
 def cantor_pairing_inverted(number: int) -> tuple:
-    pass
+    w = 8 * number + 1
+    w = sqrt(w)
+    w -= 1
+    w //= 2
+
+    t = w**2 + w
+    t /= 2
+
+    y = int(number - t)
+    x = int(w - y)
+
+    return (x, y)
+
 
 class Godel_numbering():
     def __init__(self) -> None:
         pass
-
-
-cantor_pairing_function((10, 20))
